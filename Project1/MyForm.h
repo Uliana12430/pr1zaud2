9306f4b7189b4faba6e2c21b26e7e0a2ec48aecd
ulian <ulian@JULY>
2024-10-05 18:20:45 +0300

    Добавьте файлы проекта.

---
+++ Project1/MyForm.h
@@ -0,0 +1,280 @@
+#pragma once
+
+namespace Project1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::TextBox^ textBox1;
+	protected:
+	private: System::Windows::Forms::MaskedTextBox^ maskedTextBox1;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::MaskedTextBox^ maskedTextBox2;
+	private: System::Windows::Forms::ComboBox^ comboBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->maskedTextBox1 = (gcnew System::Windows::Forms::MaskedTextBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->maskedTextBox2 = (gcnew System::Windows::Forms::MaskedTextBox());
+			this->comboBox1 = (gcnew System::Windows::Forms::ComboBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(231, 45);
+			this->textBox1->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(249, 39);
+			this->textBox1->TabIndex = 0;
+			// 
+			// maskedTextBox1
+			// 
+			this->maskedTextBox1->Location = System::Drawing::Point(231, 118);
+			this->maskedTextBox1->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
+			this->maskedTextBox1->Name = L"maskedTextBox1";
+			this->maskedTextBox1->Size = System::Drawing::Size(249, 39);
+			this->maskedTextBox1->TabIndex = 1;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(518, 45);
+			this->pictureBox1->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(606, 329);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox1->TabIndex = 2;
+			this->pictureBox1->TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(518, 45);
+			this->pictureBox2->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(606, 329);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			this->pictureBox2->TabIndex = 3;
+			this->pictureBox2->TabStop = false;
+			this->pictureBox2->Visible = false;
+			// 
+			// maskedTextBox2
+			// 
+			this->maskedTextBox2->Location = System::Drawing::Point(268, 203);
+			this->maskedTextBox2->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
+			this->maskedTextBox2->Mask = L"00/00/0000";
+			this->maskedTextBox2->Name = L"maskedTextBox2";
+			this->maskedTextBox2->Size = System::Drawing::Size(212, 39);
+			this->maskedTextBox2->TabIndex = 4;
+			this->maskedTextBox2->ValidatingType = System::DateTime::typeid;
+			// 
+			// comboBox1
+			// 
+			this->comboBox1->FormattingEnabled = true;
+			this->comboBox1->Items->AddRange(gcnew cli::array< System::Object^  >(5) { L"Україна", L"Англія", L"Італія", L"Греція", L"Німечина" });
+			this->comboBox1->Location = System::Drawing::Point(180, 285);
+			this->comboBox1->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
+			this->comboBox1->Name = L"comboBox1";
+			this->comboBox1->Size = System::Drawing::Size(300, 39);
+			this->comboBox1->TabIndex = 5;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(180, 386);
+			this->textBox2->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
+			this->textBox2->Multiline = true;
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
+			this->textBox2->Size = System::Drawing::Size(300, 72);
+			this->textBox2->TabIndex = 6;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Palatino Linotype", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(44, 56);
+			this->label1->Margin = System::Windows::Forms::Padding(5, 0, 5, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(119, 31);
+			this->label1->TabIndex = 7;
+			this->label1->Text = L"Прізвище";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Palatino Linotype", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(44, 130);
+			this->label2->Margin = System::Windows::Forms::Padding(5, 0, 5, 0);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(53, 31);
+			this->label2->TabIndex = 8;
+			this->label2->Text = L"Ім\'я";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Palatino Linotype", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(44, 215);
+			this->label3->Margin = System::Windows::Forms::Padding(5, 0, 5, 0);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(207, 31);
+			this->label3->TabIndex = 9;
+			this->label3->Text = L"Дата народження";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Palatino Linotype", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(44, 300);
+			this->label4->Margin = System::Windows::Forms::Padding(5, 0, 5, 0);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(88, 31);
+			this->label4->TabIndex = 10;
+			this->label4->Text = L"Країна";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Palatino Linotype", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(44, 386);
+			this->label5->Margin = System::Windows::Forms::Padding(5, 0, 5, 0);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(93, 31);
+			this->label5->TabIndex = 11;
+			this->label5->Text = L"Адреса";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(674, 386);
+			this->button1->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(131, 45);
+			this->button1->TabIndex = 12;
+			this->button1->Text = L"<=";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(831, 386);
+			this->button2->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(131, 45);
+			this->button2->TabIndex = 13;
+			this->button2->Text = L"=>";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(14, 31);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1131, 477);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->comboBox1);
+			this->Controls->Add(this->maskedTextBox2);
+			this->Controls->Add(this->maskedTextBox1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->pictureBox2);
+			this->Font = (gcnew System::Drawing::Font(L"Palatino Linotype", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->Margin = System::Windows::Forms::Padding(5, 6, 5, 6);
+			this->Name = L"MyForm";
+			this->Text = L"Анкета";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		pictureBox1->Visible = true;
+		pictureBox2->Visible = false;
+	}
+private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	pictureBox2->Visible = true;
+	pictureBox1->Visible = false;
+}
+private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+}
